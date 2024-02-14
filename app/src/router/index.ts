e3b8e531ed20a27de860a27106ed1452f5fec81d
Taran Pierce <taran.tpdesign@gmail.com>
2024-02-13 18:07:58 -0700

    feature/fix-heading-issue
    added some new content

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -18,12 +18,36 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/skills',
-      name: 'skills',
+      path: '/projects/',
+      name: '/projects/',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SkillsView.vue')
+      component: () => import('../views/projects/ProjectsIndexView.vue')
+    },
+    {
+      path: '/projects/caddo-lake-bayou-tours',
+      name: '/projects/caddo-lake-bayou-tours',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/projects/CaddoLakeView.vue')
+    },
+    {
+      path: '/projects/taranitup-com',
+      name: '/projects/taranitup-com',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/projects/TaranItUpView.vue')
+    },
+    {
+      path: '/projects/taranpierce-com',
+      name: '/projects/taranpierce-com',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/projects/TaranPierceView.vue')
     },
   ]
 })
